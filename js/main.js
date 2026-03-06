cd812852015e8c884186011afe37fe53fbecfe44
WYCO Nexus <55662767+WYCONexus@users.noreply.github.com>
2026-03-06 09:04:30 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+const currentPage = window.location.pathname.split('/').pop() || 'index.html';
 
+document.querySelectorAll('[data-nav]').forEach((link) => {
+  const href = link.getAttribute('href');
+  if (href === currentPage) {
+    link.classList.add('active');
+  }
+});

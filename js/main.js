2b54be97dab39799680333b05844b9a1a5b31588
WYCO Nexus <55662767+WYCONexus@users.noreply.github.com>
2026-03-06 18:48:44 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ document.querySelectorAll('[data-nav]').forEach((link) => {
     link.classList.add('active');
   }
 });
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+}
